ee02941c4abffa5940118a7b3e14ff2431e3bd8e
DinhDungVan <dinhdung.van@bluetensor.ai>
2023-04-23 12:07:17 +0200

    update product_registration

--- viz.py
+++ viz.py
@@ -1,7 +1,11 @@
-import pptk
 import numpy as np
+import open3d as o3d
 
-xyz = pptk.rand(10, 3)
+pts = np.load("pcd.npy")
+f = open("test.xyzrgb", "w")
+for p in pts:
+  f.write("%.3f %.3f %.3f %.3f %.3f %.3f\n"%(p[3], p[4], p[5], p[0]/255, p[1]/255, p[2]/255))
+f.close()
 
-v = pptk.viewer(xyz)
-v.set(point_size=0.5)
+pcd = o3d.io.read_point_cloud("test.xyzrgb")
+o3d.visualization.draw_geometries([pcd])
